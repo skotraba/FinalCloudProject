4584f8349ec039960a76c5d3e7789c1de9b4f530
Shannon <skotraba@gmail.com>
2021-04-29 12:31:32 -0400

    update

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const http = require('http');
+let fs = require('fs');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    "Content-type": "text/html",
+    'Context-Type' : 'text/html'
+  });
+  fs.readFile('./public/index.html', null, function(error, data) {
+    if(error) {
+      res.writeHead(404);
+      res.write("File not found")
+    } else {
+      res.write(data);
+    }
+    res.end();
   })
-  res.end("<h1>Hello World from openshift app</h1>")
 })
 
 server.listen(8080, () => {
   console.log("Listening to requests on port 8080")
 })
+
+
+
+
+
+
+
+
+// const http = require('http');
+// let fs = require('fs');
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {
+//     "Content-type": "text/html",
+//   })
+//   res.end("<h1>Hello World from openshift app</h1>")
+// })
+
+// server.listen(8080, () => {
+//   console.log("Listening to requests on port 8080")
+// })
